1d14271919d9f195b4723da757f0d22801d5e250
Justin Kearse <kjmatthew1@catamount.wcu.edu>
2020-06-29 08:31:40 -0400

    Wrote tests for methods for finding roots of Complex numbers.
    Trying to excercise some red-green-refactor TDD style coding, so I wrote out these three test blocks to give me a target to work towards.

--- app/src/test/java/self/kearse/mathapp/ComplexUnitTest.java
+++ app/src/test/java/self/kearse/mathapp/ComplexUnitTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -66,6 +68,77 @@ public class ComplexUnitTest {
             assertTrue( (realError < TOLERANCE) && (imaginaryError < TOLERANCE),
                     String.format("Maximum error: %s, real: %s, imaginary: %s",
                             TOLERANCE, realError, imaginaryError));
+        }
+        @Nested
+        @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+        public class powTest {
+            Complex<Double> i;
+            @BeforeAll
+            public void setup() {
+                i = new ComplexDoubleCartesian(0d, 1d);
+            }
+            @Test
+            public void iSquared() {
+                assertEquals(new ComplexDoubleCartesian(-1d, 0d),
+                        i.pow(new ComplexDoubleCartesian(2d,0d)),
+                        "i squared is -1");
+            }
+            @Test
+            public void iFourth() {
+                assertEquals(new ComplexDoubleCartesian(1d, 0d),
+                        i.pow(new ComplexDoubleCartesian(4d,0d)),
+                        "i^4 is 1");
+            }
+        }
+        @Nested
+        public class rootTest {
+            @Test
+            public void squareRootNegativeOne() {
+                assertEquals(new ComplexDoubleCartesian(0d, 1d),
+                        Complex.root(new ComplexDoubleCartesian(-1d, 0d), 2),
+                        "The principal square root of -1 is i");
+            }
+            @Test
+            public void cubeRootNegativei() {
+                /* I know, this isn't the greatest test... */
+                assertNotEquals(new ComplexDoubleCartesian(0d, 1d),
+                        Complex.root(new ComplexDoubleCartesian(0d, -1d), 3),
+                        "The principal cube root of -i is not i");
+            }
+            @Test
+            public void fourthRootNegativeSixteen() {
+                assertEquals(new ComplexDoublePolar(Math.PI / 4d, 2d),
+                        Complex.root(new ComplexDoubleCartesian(-16d, 0d), 4),
+                        "The fourth root of -16 is 2e^(i*pi/4)");
+            }
+        }
+        @Nested
+        @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+        public class rootsTest {
+            int quarterPiCount = 0;
+            List<Complex<Double>> list;
+            @BeforeAll
+            public void generateRoots() {
+                Complex<Double> quarterPi = new ComplexDoublePolar(Math.PI / 4d, 1d);
+                list = Complex.roots(new ComplexDoubleCartesian(-1d, 0d), 4);
+                for (Complex<Double> each : list) {
+                    if (quarterPi.equals(each)) {
+                        quarterPiCount += 1;
+                    }
+                }
+            }
+            @Test
+            public void hasCorrectPrincipal() {
+                assertTrue(quarterPiCount > 0, "roots List contains pi/4");
+            }
+            @Test
+            public void hasUniqueRoots() {
+                assertTrue(quarterPiCount == 1, "roots List contains unique roots");
+            }
+            @Test
+            public void hasCorrectCount() {
+                assertTrue(list.size() == 4, "There should be 4 fourth roots");
+            }
         }
     }
 }
