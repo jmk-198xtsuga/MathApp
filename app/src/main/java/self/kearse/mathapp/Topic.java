15aa79c164f3993dcb1bcc429f037e9d6321abaa
Justin Kearse <kjmatthew1@catamount.wcu.edu>
2020-08-20 00:20:48 -0400

    Rolling out a Topic class with some generic stuff and better designs to (hopefully) provide a much more robust way for listing and selecting the different topics for the app.
    This is currently intended to provide 4 things: 1- the name of each topic, for a user-facing list; 2- a Fragment class to display each topic with; 3- a reference to a saved state so that they can be persisted beyond a single activity (eventually will move this to a stored database); and 4- a List of the topics to be used in a ListView or RecyclerView in the Android UI (as well as other possible uses).
    
    In this version, some things that I have purposefully done:
    1 - I have sprinkled @NonNull annotation for appropriate fields because this is a wise designation and appears to mitigate repetitive null checks.
    2 - I have used generic typing for the Topic class instances so that 1: each of them will be coerced to use a consistent TopicFragment class for the various variables and method return types.  The intention is not that you must specify Topic<T> everywhere used, but that any use of Topic is compliant with TopicFragment specs and also able to portray its specific TopicFragment subclass.  Type inference appears to work quite well.
    3 - I'm wanting to bring the OnTopicSelectedListener interface into Topic, that just seems appropriate.
    4 - So far this is all conceptual design, I'll soon move to break down the MainActivity and fragment-handling code so that they reference this TopicList, and go from there.  This conversion is the code part of what's sandbagged me so long and delayed forward progress, I want to make sure that that conversion results in a clean and orderly product.  And figuring out how to handle the Fragments and other things has been... interesting.  I mean, I've used them before, but to make them as dynamic as I'd like is making me read and learn over a lot of new material.

---
+++ app/src/main/java/self/kearse/mathapp/Topic.java
@@ -0,0 +1,96 @@
+package self.kearse.mathapp;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Minimum specifications for topics to be presented in this app.  Requires a name, a Fragment
+ * class, and a reference for a persistent saved state.  Also builds and maintains a list of all
+ * current topics for the app.
+ * @param <F> A Fragment class for displaying the topic in an Android UI
+ */
+public class Topic<F extends TopicFragment> {
+    @NonNull private String name;
+    @NonNull private Class<F> fragmentClass;
+    private Bundle savedInstanceState;
+    private static List<Topic<? extends TopicFragment>> allTopics;
+
+    /**
+     * Establishes a new Topic with a given name, Fragment class, and saved state
+     * @param name The name of the topic, as it will appear in a user-facing list
+     * @param fragmentClass The Fragment used to display the topic
+     * @param savedInstanceState The necessary information to reproduce the fragment if it closes
+     */
+    private Topic(@NonNull String name, @NonNull Class<F> fragmentClass, Bundle savedInstanceState) {
+        this.name = name;
+        this.fragmentClass = fragmentClass;
+        this.savedInstanceState = savedInstanceState;
+    }
+
+    /**
+     * Establishes a new Topic with a given name, Fragment class, and saved state
+     * @param name The name of the topic, as it will appear in a user-facing list
+     * @param fragmentClass The Fragment used to display the topic
+     */
+    private Topic(@NonNull String name, @NonNull Class<F> fragmentClass) {
+        this(name, fragmentClass, null);
+    }
+
+    /**
+     * @return The name of the topic
+     */
+    @NonNull
+    protected String getName() {
+        return this.name;
+    }
+
+    /**
+     * @return The topic's text representation, currently its name
+     */
+    @Override
+    @NonNull
+    public String toString() {
+        return getName();
+    }
+
+    @NonNull
+    public Class<F> getFragmentClass() {
+        return this.fragmentClass;
+    }
+
+    void setSavedInstanceState(Bundle savedInstanceState) {
+        this.savedInstanceState = savedInstanceState;
+    }
+
+    Bundle getSavedInstanceState() {
+        return this.savedInstanceState;
+    }
+
+    public interface OnSelectTopicListener {
+        void onSelectTopic(int topic);
+    }
+
+    public static List<Topic<? extends TopicFragment>> getTopicList() {
+        if (allTopics == null) {
+            createTopicsList();
+        }
+        return allTopics;
+    }
+
+    private static void createTopicsList() {
+        // By intention the list of topics, itself, is built in a Singleton pattern
+        allTopics = new ArrayList<>();
+        allTopics.add(new Topic<>("Introduction",
+                IntroductionFragment.class));
+        allTopics.add(new Topic<>("Numerical Representation",
+                NumericalRepresentationFragment.class));
+//        allTopics.add(new Topic("Complex Plane", ComplexPlaneFragment.class));
+//        allTopics.add(new Topic("Basic Operations", BasicOperationsFragment.class));
+    }
+
+    //TODO: Figure out a way to correctly handle fragment class instances.  Would not want to have
+    // each of the classes persist the entire life of the application (defeats the Android lifecycle
+    // design), but still retain the savedInstanceState.
+}
