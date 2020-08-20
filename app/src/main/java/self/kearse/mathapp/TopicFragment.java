ab67c199ee147fe02848c472a51d8dd5065f0f9f
Justin Kearse <kjmatthew1@catamount.wcu.edu>
2020-08-20 00:34:07 -0400

    Rolling out a Topic class with some generic stuff and better designs to (hopefully) provide a much more robust way for listing and selecting the different topics for the app.
    This specifically is the abstract contract for any TopicFragment classes, to ensure that they have the appropriate methods to update for a stored state and have a minimum level of construction, and this onCreate should instead pull from the Topic List that is part of the Topic class.  Will see how things go.  I'll next be actively working on this code, one to build a dummy fragment to check for state persistence and to verify that the creation process is appropriate for both large screen (nested fragments) and small screen (separate fragment activities), and that the transactions don't duplicate a fragment somewhere (currently if you click on Intro or Numerical several times then it adds multiple references to the stack).

--- app/src/main/java/self/kearse/mathapp/TopicFragment.java
+++ app/src/main/java/self/kearse/mathapp/TopicFragment.java
@@ -5,8 +5,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 
-public class TopicFragment extends Fragment {
+public abstract class TopicFragment extends Fragment {
 
     public static String TOPIC_ID = "Topic ID";
 
@@ -16,27 +17,30 @@ public class TopicFragment extends Fragment {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_topic_fragment);
-            Fragment fragment;
-            switch (getIntent().getExtras().getInt(TOPIC_ID)) {
-                case 0:
-                    fragment = new IntroductionFragment();
-                    break;
-                case 1:
-                    fragment = new NumericalRepresentationFragment();
-                    break;
-                case 2:
-                    //fragment = new ComplexPlaneFragment();
-                    //break;
-                case 3:
-                    //fragment = new BasicOperationsFragment();
-                    //break;
-                default:
-                    fragment = new IntroductionFragment();
+            Bundle extras = getIntent().getExtras();
+            Topic<? extends TopicFragment> topic;
+            TopicFragment fragment = null;
+            Bundle topicState = null;
+            if (extras != null) {
+                topic = Topic.getTopicList().get(extras.getInt(TOPIC_ID));
+                topicState = topic.getSavedInstanceState();
+                try {
+                    fragment = topic.getFragmentClass().newInstance();
+                } catch (Exception e) {
+                    Log.e("TopicFragment", "unable to instantiate topic fragment: " +
+                            e.getMessage());
+                    fragment = null;
+                }
             }
-            fragment.setArguments(getIntent().getExtras());
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            transaction.add(R.id.fragment_container, fragment);
-            transaction.commit();
+            if (fragment != null) {
+                if (topicState != null) fragment.updateState(topicState);
+                fragment.setArguments(getIntent().getExtras());
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.add(R.id.fragment_container, fragment);
+                transaction.commit();
+            }
         }
     }
+
+    public abstract void updateState(Bundle topicState);
 }
